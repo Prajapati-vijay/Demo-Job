21993ed78b8a46d605477d87f1e5c38004ebbcfd
Vijay <vijay@finflock.com>
2025-04-18 19:11:33 +0530

    adding sys

--- test3/job.py
+++ test3/job.py
@@ -4,6 +4,12 @@ from datetime import datetime
 import time
 
 print("Starting job")
+import sys
+
+# Print the arguments passed
+print(sys.arg[0])
+print(sys.arg[1])
+print(sys.arg[2])
 time.sleep(160)
 # Get the current time
 current_time = datetime.now()
